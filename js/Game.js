b7bc1476d3842336769a3bd78034ed8c4ecb6b9a
Cody Reed <cody2j2reed@gmail.com>
2018-12-08 21:56:39 -0500

    Update js/Game.js
    Added constructor method along with properties: players, board, ready.
    
    Both this.board and this.players invoke class creator calls

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,15 @@
-class Game {}
+class Game {
+  constructor() {
+    this.players = this.createPlayers();
+    this.board = new Board();
+    this.ready = false;
+  }
+
+  createPlayers() {
+    const players = [
+      new Player("indigo", 1, "#e15258", true),
+      new Player("scarlett", 2, "#e59a13")
+    ];
+    return players;
+  }
+}

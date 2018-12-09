116c02a01ce8aee7757d0411aea411039dc7f7ac
Cody Reed <cody2j2reed@gmail.com>
2018-12-09 12:06:11 -0500

    Update js/Token.js
    Added drawHTMLToken() render method:
    
    created "div" element/tag and assigned to underlay variable.
    appended new element to ID attribute "game-board-underlay."
    
    setAttribute() on underlay for "id" to equal this.id, and class to equal "token."
    
    set BGColor to owners/player's color on underlay.
    
    Added GETTER method htmlToken()

--- js/Token.js
+++ js/Token.js
@@ -4,4 +4,17 @@ class Token {
     this.id = `token-${index}-${owner.id}`;
     this.dropped = false;
   }
+
+  drawHTMLToken() {
+    const underlay = document.createElement("div");
+
+    document.getElementById("game-board-underlay").appendChild(underlay);
+
+    underlay.setAttribute("id", this.id);
+    underlay
+      .setAttribute("class", "token")
+      .style.backGroundColor(this.owner.color);
+  }
+
+  get htmlToken() {}
 }
